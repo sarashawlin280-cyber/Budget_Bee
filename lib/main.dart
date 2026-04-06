73ab1228b2d414fcfe62986f02644898581af267
Sadia-Afrin-Tisha <sadiaafrintisha729@gmail.com>
2026-04-06 21:47:42 +0600

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/home_page/log_in.dart';
 
-void main() {
-  runApp(const BudgetBeeApp());
+void main(){
+  runApp(BudgetBeeApp());
 }
 
-class BudgetBeeApp extends StatelessWidget {
-  const BudgetBeeApp({super.key});
+class BudgetBeeApp extends StatelessWidget{
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BudgetBee',
-      // Standardizing the dark theme across the app
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.orange,
-        scaffoldBackgroundColor: Colors.black,
-        colorScheme: const ColorScheme.dark(
-          primary: Colors.orangeAccent,
-          secondary: Colors.amber,
-        ),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
+      debugShowCheckedModeBanner:false,
+      home: log_inPage(),
     );
   }
 }
